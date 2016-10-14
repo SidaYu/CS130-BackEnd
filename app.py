e8bc179f3ea435659106d37005d1479626bd8f9f
Sida.Yu <sidayu92@gmail.com>
2016-10-13 22:20:58 -0700

    db test

--- app.py
+++ app.py
@@ -9,11 +9,16 @@ This file creates your application.
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_restful import Resource, Api
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://patrycja:mypassword@localhost/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+db = SQLAlchemy(app)
 
 
 ###
